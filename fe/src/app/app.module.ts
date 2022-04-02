bc8ecdb851790e1a26bc3b95c7df618ff8318512
mitach <malinovd02@gmail.com>
2022-04-02 08:13:57 +0200

    Restructure

--- fe/src/app/app.module.ts
+++ fe/src/app/app.module.ts
@@ -7,12 +7,18 @@ import { MatButtonModule } from '@angular/material/button'
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { HeaderComponent } from './header/header.component';
+import { HeaderComponent } from './core/header/header.component';
+import { FooterComponent } from './core/footer/footer.component';
+import { PostListComponent } from './posts/post-list/post-list.component';
+import { PostCreateComponent } from './posts/post-create/post-create.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    HeaderComponent
+    HeaderComponent,
+    FooterComponent,
+    PostListComponent,
+    PostCreateComponent,
   ],
   imports: [
     BrowserModule,
